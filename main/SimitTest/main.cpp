288835b5f201aa49ce301800da8274b960b38af3
Your Name <your.email@example.com>
2025-06-23 17:53:44 -0400

    Main commit

---
+++ main/SimitTest/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+
+#using <System.dll>
+#using <System.Core.dll>
+// Reference the GAC assembly
+#using <Siemens.Simit.API.Coupling.dll>
+
+using namespace Siemens::Simit::API::Coupling;
+
+int main() {
+
+
+
+
+    return 0;
+}
